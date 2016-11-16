ffd3e93098f852f4c39e7bfc26721f1799a252fb
Gaebrielle McIntosh <mcing079@newschool.edu>
2016-11-16 08:57:12 +0000

    icfive

--- jquery_exercisetwo/js/script.js
+++ jquery_exercisetwo/js/script.js
@@ -1,28 +1,25 @@
-$("body").css("background-color", "blue");
+$("body").css("background-color","green");
 
 for (var i=0; i < 100; i++) {
-
-$("body").append("<div class='dot'></div>"); 
-    
+    $("body").append("<div class='dot'></div>");
 }
 
 var visibility = 0;
 var myCounter = 1;
 
-var cats - ["img/cat1.jpg", ]
-$("body").append("img src ='" + cats)
+var cats = ["img/face.jpg", "img/face.jpg", "img/face.jpg"];
 
+$("body").append("<img src='" + cats[Math.floor(Math.random()*3)] + "'>");
 
 $(".dot").each(function() {
-    $(this).css("opacity", visibility/100);
-    $(this).html(myCounter);
-    $(this).css("height",myCounter +100);
-     $(this).css("width",myCounter +100);
-     $(this).css("top",Math.floor(Math.random()*$
-     (window).height()));
-    myCounter++;
-    visibility++;
-
+   $(this).css("opacity", visibility/100);
+   $(this).html(myCounter);
+   $(this).css("height",myCounter+100);
+   $(this).css("width",myCounter+100);
+   $(this).css("top",Math.floor(Math.random()*$(window).height()));
+   $(this).css("left",Math.floor(Math.random()*$(window).width()));
+   $(this).css("font-size",Math.floor(Math.random()*$(window).width()));
+   myCounter++;
+   visibility++;
+   
 });
-
-
